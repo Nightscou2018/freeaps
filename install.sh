b0a5ec0bb289026004e17cc48dc7463bb248e2f1
Ivan Valkou <zloyfey@gmail.com>
2018-05-26 01:35:15 +0300

    fix syntax

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd /root &&
 echo "oref0-online.sh"
 cp freeaps/scripts/oref0/oref0-online.sh src/oref0/bin
 
-if [ -d  .xDripAPS ] then
+if [ -d  .xDripAPS ]; then
     echo "xDripAPS.py"
     cp freeaps/scripts/xDripAPS/xDripAPS.py .xDripAPS
 fi
