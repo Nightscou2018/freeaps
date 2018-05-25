7f9cba1c2a39e242384eca21cf4b32fcef59d0fa
Ivan Valkou <zloyfey@gmail.com>
2018-05-26 01:31:38 +0300

    fix paths

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ echo "Installing FreeAPS extensions..."
 
 cd /root &&
 echo "oref0-online.sh"
-cp scripts/oref0/oref0-online.sh src/oref0/bin
+cp freeaps/scripts/oref0/oref0-online.sh src/oref0/bin
 
 if [ -d  .xDripAPS ] then
     echo "xDripAPS.py"
-    cp scripts/xDripAPS/xDripAPS.py .xDripAPS
+    cp freeaps/scripts/xDripAPS/xDripAPS.py .xDripAPS
 fi
 echo "All done!"
