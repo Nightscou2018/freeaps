df0cba9f18cd3c8cdd67d5ccdb404a2b4f829b2b
Ivan Valkou <zloyfey@gmail.com>
2018-06-30 20:35:18 +0300

    Update oref0

--- update.sh
+++ update.sh
@@ -2,7 +2,15 @@
 
 echo "Installing FreeAPS extensions..."
 
+echo "Updating oref0..."
+if git branch | grep "* master"; then
+    cd ~/src/oref0 && git checkout master && git pull
+else
+    cd ~/src/oref0 && git checkout dev && git pull
+fi
+
 cd /root &&
+echo "Patching files..."
 echo "oref0-online.sh"
 cp freeaps/scripts/oref0/oref0-online.sh src/oref0/bin
 
